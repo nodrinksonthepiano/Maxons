8e3f5061f98ea92aad9246012deafc1f8b5c2e05
Jai Tea <nodrinksonthepiano@gmail.com>
2024-04-27 11:23:41 -0500

    2nd carousel for reviews

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
-    $('#carouselExampleIndicators').on('slide.bs.carousel', function (e) {
-        var $e = $(e.relatedTarget);
-        var idx = $e.index();
-        var itemsPerSlide = 3;
-        var totalItems = $('.carousel-item').length;
+    $('.carousel').each(function() {
+        $(this).on('slide.bs.carousel', function(e) {
+            var $e = $(e.relatedTarget);
+            var idx = $e.index();
+            var itemsPerSlide = 3;
+            var totalItems = $(this).find('.carousel-item').length;
 
-        if (idx >= totalItems-(itemsPerSlide-1)) {
-            var it = itemsPerSlide - (totalItems - idx);
-            for (var i = 0; i < it; i++) {
-                // Append items
-                if (e.direction == "left") {
-                    $('.carousel-item').eq(i).appendTo('.carousel-inner');
-                } else {
-                    $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            if (idx >= totalItems - (itemsPerSlide - 1)) {
+                var it = itemsPerSlide - (totalItems - idx);
+                for (var i = 0; i < it; i++) {
+                    if (e.direction === "left") {
+                        $(this).find('.carousel-item').eq(i).appendTo(this);
+                    } else {
+                        $(this).find('.carousel-item').eq(0).appendTo(this);
+                    }
                 }
             }
-        }
+        });
     });
 });
